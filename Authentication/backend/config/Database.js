c2401a23e4fcccf61a29bcd5dae8041712becc14
Raiiynn <email.githubdaffa@gmail.com>
2024-12-23 22:39:44 +0700

    update

--- Authentication/backend/config/Database.js
+++ Authentication/backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db =  new  Sequelize('terranusaofc', 'root', '', {
+const db =  new  Sequelize('terranusa', 'root', '', {
     host:'localhost',
     dialect:'mysql'
 });
--- views/config.php
+++ views/config.php
@@ -1,7 +1,7 @@
 <?php
 // config.php
 $host = 'localhost';
-$dbname = 'terranusa';
+$dbname = 'terranusa_db';
 $username = 'root';
 $password = '';
 
